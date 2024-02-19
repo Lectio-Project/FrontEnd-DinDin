36a5fc058bb0995efe51667dbce561fb632b8146
Rochel Rodrigues Coelho <rochelrrc@gmail.com>
2024-02-19 09:14:41 -0300

    Commit de seguranca

---
+++ src/app/pages/dashboard/page.tsx
@@ -0,0 +1,15 @@
+
+
+
+export default function Dashboard(){
+
+    <main>
+        <header>
+            <img src='' alt= "Logo with the outline of a hexagon with a white line inside and a dark blue line outside, accompanied by the word Dindin"/>
+        </header>
+
+    </main>
+
+
+
+}
